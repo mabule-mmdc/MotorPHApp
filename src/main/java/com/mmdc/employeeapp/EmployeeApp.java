cf5654543e8d95e1b7e92701d4c404a9f75d540f
Micah Andrew Bule <micah.bule@gmail.com>
2023-06-13 18:22:50 +0800

    feat: add some commits

--- src/main/java/com/mmdc/employeeapp/EmployeeApp.java
+++ src/main/java/com/mmdc/employeeapp/EmployeeApp.java
@@ -185,6 +185,8 @@ public class EmployeeApp {
             
             System.out.println("Monthly total hour for month of " + monthNumber + " is " + monthlyTotalHours + " hours.");
             System.out.println("The gross monthly salary for " + selectedEmployee.getFirstName() + " " + selectedEmployee.getLastName() + " is " + selectedEmployee.getMonthlyGrossSalary(parseInt(monthNumber)));
+            
+            System.out.println("Some lines here");
         } catch (FileNotFoundException ex) {
             System.out.println("No file found");
         } catch (IOException ex) {
